012739458d0a23dce2588c879d16c68a228fec18
Atul Singh <atulsingh25@example.com>
2025-11-19 12:06:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
    import React from 'react';
    
-import TempConverter from "./TempConverter.js"
+import Calculator from "./Calculator.js"
 import './App.css'; // Assuming you have an App.css file for styling
   // Adjust the path as necessary
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <TempConverter />
+      <Calculator />
     </div>  
   );
 } 
